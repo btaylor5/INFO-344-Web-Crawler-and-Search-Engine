9207ff4944ecb7283940c4b562856f0f859d8cd7
btaylor5 <btaylor5@uw.edu>
2015-04-05 23:19:13 -0700

    Removed Passwords from config and added config to gitignore. Good thing this is private...

--- ProgrammingAssignment1/config.php
+++ ProgrammingAssignment1/config.php
@@ -7,7 +7,7 @@
  */
 
 return array(
-    'host' => 'mysql:host=mysqlinstance.cm7cqbqc6zne.us-west-2.rds.amazonaws.com;port=3306;dbname=INFO344',
-    'username' => 'info344user',
-    'password' => '4point0info344'
+    'host' => '',
+    'username' => '',
+    'password' => ''
 );
