f800152e28547efb65b4e96235845a29416592e1
btaylor5 <btaylor5@uw.edu>
2015-04-13 22:08:48 -0700

    Added better commenting

--- ProgrammingAssignment1/code/Controller/Requests.php
+++ ProgrammingAssignment1/code/Controller/Requests.php
@@ -5,7 +5,7 @@
  * Date: 4/9/15
  * Time: 7:46 PM
  *
- * Listens for requests and returns JSON representation of the query results
+ * Listens for requests and returns JSON representation of the query results.
  *
  */
 header("Content-Type: application/json", true);
@@ -14,21 +14,26 @@ include_once('../Model/DBAccess.php');
 include_once('../Model/Search.php');
 include_once('../Model/Player.php');
 
-
+// Listens for a Request with the paramater 'name'
 $DB_Connection = new DBAccess();
-if(isset($_REQUEST['name'])) {
+if(isset($_REQUEST['name']))
+{
     $player = new Search($_REQUEST['name']);
     $playerStack = $player->searchLevenshtein($DB_Connection);
     $players = $playerStack->asArray();
     $results = array();
-    if (sizeof($players) > 0) {
-        foreach($players as $player) {
+    if (sizeof($players) > 0)
+    {
+        foreach($players as $player)
+        {
             $results[] = $player->arrayRepresentation();
         }
         echo json_encode(array_reverse($results));
-    } else {
+    } else
+    {
         echo "No Results, Try Refining your search!";
     }
-} else {
+} else
+{
     echo "{ERROR}";
 }
